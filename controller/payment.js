9c0d81a5eeb24e3d6a234427fc48aee5f6f82d01
OlachiFaith <okereolachifaith258@gmail.com>
2026-06-27 10:24:26 +0100

    updated the verify payment

--- controller/payment.js
+++ controller/payment.js
@@ -61,7 +61,7 @@ exports.initiatePayment = async (req, res, next) => {
         email: mother.email,
         name: name
       },
-      redirect_url: "/fundsSuccess",
+      redirect_url: "https://maternal-path-fe.vercel.app/fundsSuccess",
       currency: 'NGN',
       reference: reference
     };
